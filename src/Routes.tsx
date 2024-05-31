abff049965d571b67a5de964c7263de1ebc20001
amine takdenti <aminetakdenti4@gmail.com>
2024-05-31 23:47:04 +0100

    fix vercel.json file and dynamic route stuff in vercel

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,23 +7,24 @@ import { SignedIn } from "@clerk/clerk-react";
 import { Route, Routes as Router } from "react-router-dom";
 
 function Routes() {
-	return (
-		<Router>
-			<Route path="/" element={<Layout />}>
-				<Route index element={<Home />} />
-				<Route
-					path="/upload"
-					element={
-						<SignedIn>
-							<Upload />
-						</SignedIn>
-					}
-				/>
-				<Route path="/blog/:id" element={<BlogPage />} />
-				<Route path="/user/:id" element={<User />} />
-			</Route>
-		</Router>
-	);
+  return (
+    <Router>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="/upload"
+          element={
+            <SignedIn>
+              <Upload />
+            </SignedIn>
+          }
+        />
+        <Route path="/blog/:id" element={<BlogPage />} />
+        <Route path="/user/:id" element={<User />} />
+      </Route>
+      <Route path="*" element={<div>Not Found</div>} />
+    </Router>
+  );
 }
 
 export default Routes;
